61b5d6e9b35018b7a7fa5f8357fd53a33916760b
jimin990 <wlals3236@naver.com>
2025-01-24 17:09:09 +0900

    1월 23일 공부

--- src/main/java/praticeServlet/praticeServlet/web/springmvc/old/OldController.java
+++ src/main/java/praticeServlet/praticeServlet/web/springmvc/old/OldController.java
@@ -10,8 +10,7 @@ import org.springframework.web.servlet.mvc.Controller;
 public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("request = " + request);
-
-        return null;
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
     }
 }

fe896d470ada175b18112594ee7fa1221322d8aa
jimin990 <wlals3236@naver.com>
2025-02-06 16:43:13 +0900

    jpa domain design

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY,mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+
+    /**
+     * enum을 사용할때 주의!
+     * enumerated를 붙혀주고, enumType을 string으로 바꿔줘야한다.
+     * 기본타입이 ordinary인데 이건 숫자로 입력이 되기 때문에 중간에 값이 들어가면, 원래 3인 상태가 밀려나서 망할 수 있다
+     */
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus; //READY, COMP
+
+}
